f7f53cfe0326c44f74e3c8fdaaaa86b9bc0ce60e
Ing-wen <36122633+Cyber-Ingwen@users.noreply.github.com>
2022-11-29 02:08:38 +0800

    Update videoHelper.py

--- videoHelper.py
+++ videoHelper.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
-# version 4
+# version 4.1
 # developed by zk chen
+#原地址请看readme
+#二次修改 hewei2723
+#三次修改 Cyber-Ingwen
+#此源码适用于雨课堂
 import time
 import requests
 import re
 import json
 
-# 以下的csrftoken和sessionid需要改成自己登录后的cookie中对应的字段！！！！而且脚本需在登录雨课堂状态下使用
-# 登录上雨课堂，然后按F12-->选Application-->找到雨课堂的cookies，寻找csrftoken和sessionid字段，并复制到下面两行即可
-csrftoken = "yours" #需改成自己的
-sessionid = "yours" #需改成自己的
+cookie = '' # 你的cookie
+csrftoken = cookie[cookie.index('csrftoken') + len('csrftoken='):]
+csrftoken = csrftoken[:csrftoken.index(';')]
+# sessionid = '' # expired
 
-# 以下字段不用改，下面的代码也不用改动
-user_id = ""
-
+select = True 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
     'Content-Type': 'application/json',
-    'Cookie': 'csrftoken=' + csrftoken + '; sessionid=' + sessionid + '; university_id=3078; platform_id=3',
+    'Cookie': cookie,
     'x-csrftoken': csrftoken,
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
-    'university-id': '3078',
+    'university-id': '2930',
     'xtbz': 'cloud'
 }
 
@@ -34,11 +36,13 @@ leaf_type = {
     "discussion": 4
 }
 
+课程id = []
+章节map = {}
 def one_video_watcher(video_id,video_name,cid,user_id,classroomid,skuid):
     video_id = str(video_id)
     classroomid = str(classroomid)
-    url = "https://gsscut.yuketang.cn/video-log/heartbeat/"
-    get_url = "https://gsscut.yuketang.cn/video-log/get_video_watch_progress/?cid="+str(cid)+"&user_id="+user_id+"&classroom_id="+classroomid+"&video_type=video&vtype=rate&video_id=" + str(video_id) + "&snapshot=1&term=latest&uv_id=3078"
+    url = "https://www.yuketang.cn/video-log/heartbeat/"
+    get_url = "https://www.yuketang.cn/video-log/get_video_watch_progress/?cid="+str(cid)+"&user_id="+user_id+"&classroom_id="+classroomid+"&video_type=video&vtype=rate&video_id=" + str(video_id) + "&snapshot=1&term=latest&uv_id=3078"
     progress = requests.get(url=get_url, headers=headers)
     if_completed = '0'
     try:
@@ -88,7 +92,7 @@ def one_video_watcher(video_id,video_name,cid,user_id,classroomid,skuid):
             timestap = min(max_time, timestap+1000*15)
         data = {"heart_data": heart_data}
         r = requests.post(url=url,headers=headers,json=data)
-        print(r.text)
+        # print(r.text)
         try:
             error_msg = json.loads(r.text)["message"]
             if "anomaly" in error_msg:
@@ -101,6 +105,7 @@ def one_video_watcher(video_id,video_name,cid,user_id,classroomid,skuid):
             time.sleep(float(delay_time) + 0.5)
             video_frame = 0
             print("恢复工作啦～～")
+            submit_url = "https://www.yuketang.cn/mooc-api/v1/lms/exercise/problem_apply/?term=latest&uv_id=3078"
             r = requests.post(url=submit_url, headers=headers, data=data)
         except:
             pass
@@ -114,71 +119,57 @@ def one_video_watcher(video_id,video_name,cid,user_id,classroomid,skuid):
     print("视频"+video_id+" "+video_name+"学习完成！")
     return 1
 
-def get_videos_ids(course_name,classroom_id,course_sign):
-    get_homework_ids = "https://gsscut.yuketang.cn/mooc-api/v1/lms/learn/course/chapter?cid="+str(classroom_id)+"&term=latest&uv_id=3078&sign="+course_sign
-    homework_ids_response = requests.get(url=get_homework_ids, headers=headers)
-    homework_json = json.loads(homework_ids_response.text)
-    homework_dic = {}
-    try:
-        for i in homework_json["data"]["course_chapter"]:
-            for j in i["section_leaf_list"]:
-                if "leaf_list" in j:
-                    for z in j["leaf_list"]:
-                        if z['leaf_type'] == leaf_type["video"]:
-                            homework_dic[z["id"]] = z["name"]
-                else:
-                    if j['leaf_type'] == leaf_type["video"]:
-                        # homework_ids.append(j["id"])
-                        homework_dic[j["id"]] = j["name"]
-        print(course_name+"共有"+str(len(homework_dic))+"个作业喔！")
-        return homework_dic
-    except:
-        print("fail while getting homework_ids!!! please re-run this program!")
-        raise Exception("fail while getting homework_ids!!! please re-run this program!")
-
 if __name__ == "__main__":
-    your_courses = []
+    # 获取userid
+    response = requests.get('https://www.yuketang.cn/v2/api/web/userinfo', headers=headers).text
+    user_id = re.search(r'"user_id":(.*?),', response).group(1)
+    params = {
+        'identity': '2',
+    }
+    response = requests.get('https://www.yuketang.cn/v2/api/web/courses/list', params=params,headers=headers).json()
+    if response['errmsg'] != 'Success':
+        print("csrftoken或者sessionid有问题请检查！")
+        exit(1)
+    index = 0
+    # 获取课程id 和 课程名字
+    for i in response['data']['list']:
+        课程id.append(i['classroom_id'])
+        print("编号：" + str(index + 1) + " 课名：" + str(i["course"]['name']))
+        index += 1
+    number = int(input("你想刷哪门课呢？请输入编号：\n"))
+    cid = str(课程id[number-1])
+    url = 'https://www.yuketang.cn/v2/api/web/logs/learn/%s?actype=-1&page=0&offset=20&sort=-1'% cid
+    response = requests.get(url,headers=headers).json()
+    if response['data']['prev_id'] == -1:
+        print('该课程尚无内容！程序退出....')
+        exit(1)
+    if response['errcode'] != 0:
+        print(response)
+        exit(1)
+    courseware_id = []
+    for i in response['data']['activities']:
+        courseware_id.append(i['courseware_id'])
+        章节map[i['courseware_id']] = i['title']
+    data = {
+        'cid': cid,
+        'new_id':courseware_id
+    }
 
-    # 首先要获取用户的个人ID，即user_id,该值在查询用户的视频进度时需要使用
-    user_id_url = "https://gsscut.yuketang.cn/edu_admin/check_user_session/"
-    id_response = requests.get(url=user_id_url, headers=headers)
-    try:
-        user_id = re.search(r'"user_id":(.+?)}', id_response.text).group(1).strip()
-    except:
-        print("也许是网路问题，获取不了user_id,请试着重新运行")
-        raise Exception("也许是网路问题，获取不了user_id,请试着重新运行!!! please re-run this program!")
-
-    # 然后要获取教室id
-    get_classroom_id = "https://gsscut.yuketang.cn/mooc-api/v1/lms/user/user-courses/?status=1&page=1&no_page=1&term=latest&uv_id=3078"
-    submit_url = "https://gsscut.yuketang.cn/mooc-api/v1/lms/exercise/problem_apply/?term=latest&uv_id=3078"
-    classroom_id_response = requests.get(url=get_classroom_id, headers=headers)
-    try:
-        for ins in json.loads(classroom_id_response.text)["data"]["product_list"]:
-            your_courses.append({
-                "course_name": ins["course_name"],
-                "classroom_id": ins["classroom_id"],
-                "course_sign": ins["course_sign"],
-                "sku_id": ins["sku_id"],
-                "course_id": ins["course_id"]
-            })
-    except Exception as e:
-        print("fail while getting classroom_id!!! please re-run this program!")
-        raise Exception("fail while getting classroom_id!!! please re-run this program!")
-
-    # 显示用户提示
-    for index, value in enumerate(your_courses):
-        print("编号："+str(index+1)+" 课名："+str(value["course_name"]))
-    number = input("你想刷哪门课呢？请输入编号。输入0表示全部课程都刷一遍\n")
-    if int(number)==0:
-        #0 表示全部刷一遍
-        for ins in your_courses:
-            homework_dic = get_videos_ids(ins["course_name"],ins["classroom_id"], ins["course_sign"])
-            for one_video in homework_dic.items():
-                one_video_watcher(one_video[0],one_video[1],ins["course_id"],user_id,ins["classroom_id"],ins["sku_id"])
-    else:
-        #指定序号的课程刷一遍
-        number = int(number)-1
-        homework_dic = get_videos_ids(your_courses[number]["course_name"],your_courses[number]["classroom_id"],your_courses[number]["course_sign"])
-        for one_video in homework_dic.items():
-            one_video_watcher(one_video[0], one_video[1], your_courses[number]["course_id"], user_id, your_courses[number]["classroom_id"],
-                                your_courses[number]["sku_id"])
+    headers['classroom-id'] = cid
+    headers['xtbz'] = 'ykt'
+    headers['Referer'] = 'https://www.yuketang.cn/v2/web/studentLog/%s'%cid
+    if select:
+        skuid = requests.get(url='https://www.yuketang.cn/v2/api/web/classrooms/%s?role=5' % cid,headers=headers).json()['data']['free_sku_id']
+        get_url = 'https://www.yuketang.cn/c27/online_courseware/schedule/score_detail/single/%s/0/' % skuid
+        ret = requests.get(url=get_url, headers=headers).json()
+        for i in ret['data']['leaf_level_infos']:
+            if i['leaf_type'] == leaf_type['homework']:
+                continue
+            get_url = 'https://www.yuketang.cn/mooc-api/v1/lms/learn/leaf_info/%s/%s/' % (cid, i['id'])
+            getccid = requests.get(url=get_url, headers=headers).json()
+            skuid = getccid['data']['sku_id']
+            user_id = getccid['data']['user_id']
+            ccid = getccid['data']['content_info']['media']['ccid']
+            course_id = getccid['data']['course_id']
+            one_video_watcher(i['id'], i['leaf_chapter_title'], course_id, str(user_id), cid, skuid)
+        print("---------------------------已完成-------------------------------")
